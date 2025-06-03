b39b2adfd7066178a752a535f8f58d6729dbd6de
shrujal <shrujaldoshi907@gmail.com>
2025-06-03 19:32:11 +0530

    Learn Function

---
+++ Functions/app.js
@@ -0,0 +1,98 @@
+// Practice que 1 : Roll a dice & show a numver between 1 to 6 using function
+
+// function rollDice()
+// {
+//     let num = Math.floor(Math.random()*6  + 1);
+//     console.log(num);
+// }
+// rollDice();
+
+// ----------------------------------------------------------------------------------------
+
+// Example 1 : Function  with parameter
+
+// function userInfo(name , age)
+// {
+//     console.log(`${name}'s age is ${age}`);
+// }
+// userInfo("shrujal",18);
+
+// ----------------------------------------------------------------------------------------
+
+// Practice que 2 : Avarge of 3 number using function
+
+// function Avg(a,b,c)
+// {
+//     console.log((a+b+c)/3);
+// }
+// Avg(3,4,5);
+
+// ----------------------------------------------------------------------------------------
+
+// Practice que 3 : Crating a multiplication number using function
+
+// function mulTable(num)
+// {
+//     for(let i=1;i<=10;i++)
+//     {
+//         console.log(`${num} * ${i} =`,num*i);
+//     }
+// }
+// mulTable(9);
+
+// ----------------------------------------------------------------------------------------
+
+// Practice que 4 : Creating a function that return concatenation of sting in array
+
+// let str = ["I"," ","am"," ","shrujal"," ","doahi"," ","!"];
+
+// function concat(str)
+// {
+//     let ans = "";
+//     for(let i=0;i<str.length;i++)
+//     {
+//         ans += str[i];
+//     }
+//     return ans;
+// }
+
+// ----------------------------------------------------------------------------------------
+
+// Example 2 : higher order Function 
+
+// function mulGreeting(fun , count)
+// {
+//     for(let i=0;i<=count;i++)
+//     {
+//         fun();
+//     }
+// }
+
+// let Greeting = function()
+// {
+//     console.log("Hello !");
+// }
+
+// mulGreeting(Greeting,10);
+
+// ----------------------------------------------------------------------------------------
+
+// Example 3 : Methods 
+
+// const calculator = {
+//     add(a,b)
+//     {
+//         console.log(a+b);
+//     },
+//     sub(a,b)
+//     {
+//         console.log(a-b);
+//     },
+//     mul(a,b)
+//     {
+//         console.log(a*b);
+//     }
+// };
+
+
+

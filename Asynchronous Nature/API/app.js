3b414cabc443295a201a394529494c27db9dcec3
shrujal <shrujaldoshi907@gmail.com>
2025-06-13 02:28:17 +0530

    Learn about axios

--- Asynchronous Nature/API/app.js
+++ Asynchronous Nature/API/app.js
@@ -63,3 +63,101 @@
 //         console.log("error caught:",err);
 //     }  
 // }
+
+// ---------------------------------------------------------------------------------------
+
+// Example 4 : Using Axios
+
+// let url = "https://catfact.ninja/fact";
+
+// async function getFacts() {
+//     try {
+//         let res = await axios.get(url);
+//         console.log(res.data);
+//     } 
+//     catch (err) {
+//         console.log("error caught:",err);
+//     }  
+// }
+
+// ---------------------------------------------------------------------------------------
+
+// Example 5 : Using Axios with html
+
+// let btn = document.querySelector("button");
+
+// btn.addEventListener("click",async () => {
+//     console.log("Button was pressed");
+//     let Fact = await getFacts();
+//     console.log(Fact);
+//     let p = document.querySelector("p");
+//     p.innerText = Fact;
+// });
+
+// let url = "https://catfact.ninja/fact";
+
+// async function getFacts() {
+//     try {
+//         let res = await axios.get(url);
+//         return res.data.fact;
+//     } 
+//     catch (err) {
+//         console.log("error caught:",err);
+//         return "No fact found";
+//     }  
+// }
+
+// ---------------------------------------------------------------------------------------
+
+// Example 6 : Sending Headers with API Request
+
+// const url = "https://icanhazdadjoke.com/";
+
+// async function getJokes() {
+//     try {
+//         const config = {headers: {Accept: "application/json"}};
+//         let res = await axios.get(url,config);
+//         console.log(res.data);
+//     } 
+//     catch (error) {
+//         console.log(error);
+//     }
+// }
+
+// ---------------------------------------------------------------------------------------
+
+// Example 7 : Activity using query string
+
+// let btn = document.querySelector("button");
+
+//     btn.addEventListener("click", async () => {
+//         let country = document.querySelector("input").value.trim();
+//         let universities = await getClg(country);
+        
+//         let ul = document.querySelector("#result");
+//         ul.innerHTML = ""; 
+
+//         if (universities.length > 0) {
+//             universities.forEach(uni => {
+//                 let li = document.createElement("li");
+//                 li.innerText = uni.name;
+//                 ul.appendChild(li);
+//             });
+//         } else {
+//             let li = document.createElement("li");
+//             li.innerText = "No universities found.";
+//             ul.appendChild(li);
+//         }
+//     });
+
+//     let url = "http://universities.hipolabs.com/search?country=";
+
+//     async function getClg(country) {
+//         try {
+//             let res = await axios.get(url + country);
+//             return res.data;
+//         } catch (error) {
+//             console.log(error);
+//             return [];
+//         }
+//     }
